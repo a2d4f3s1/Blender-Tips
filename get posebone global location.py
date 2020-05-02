1cfbe18ede3d1e22bd834ddd43ca39f33356f5e2
a2d4f3s1 <a2d4f3s1@gmail.com>
2020-05-02 18:42:37 +0900

    no message

--- get posebone global location.py
+++ get posebone global location.py
@@ -1,5 +1,5 @@
 import bpy
 p = bpy.context.active_pose_bone  # Our pose bone
 o = bpy.context.object            # Our armature object
-global_location = o.matrix_world @ p.matrix @ p.location
+global_location = o.matrix_world @ p.matrix @ p.location #以前は*が使えたようだがどこかで変わったらしい
 print (global_location)
